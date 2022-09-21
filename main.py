61f9c275fdf2585dd67d93376fbd3c9677029d96
Sour3 <31393967+Sour-o7@users.noreply.github.com>
2022-09-20 22:55:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import threading
 
 #Naval Version 2.0.0
 #9/20/2022
-
-client = commands.Bot(command_prefix="++", intents=discord.Intents.all())
+activity = discord.Activity(type=discord.ActivityType.playing, name="ZephyrRP")
+client = commands.Bot(command_prefix="++", intents=discord.Intents.all(), activity = activity)
 
 @client.event
 async def on_ready():
@@ -18,4 +18,4 @@ async def on_ready():
     for f in os.listdir("./cogs"):
         if f.endswith(".py"):
             await client.load_extension("cogs." + f[:-3])
-client.run('TOKEN')
+client.run('ODQ5MTMzODIwOTQ2OTM5OTc0.YLWvfw.SlPvoOPHhilB64uJJzrJqMm597s')
